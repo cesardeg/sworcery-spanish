8ccdd5cb5817feacf8d10d4bbe85d93747512fb6
César Delgado <cesardeg@Zigattas-MBP.local>
2023-07-12 23:28:17 -0600

    fixed script to repackage

--- steam/build.sh
+++ steam/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the directory of the script
-root=$(dirname "$(realpath "$0")")
+script_dir=$(dirname "$(realpath "$0")")
 
 # Validate the number of arguments
 if [ $# -lt 1 ]; then
@@ -29,7 +29,7 @@ case "$locale" in
 esac
 
 # Set the res dir based on the script directory and locale
-res_dir="${root}/../build/steam/$locale_dir"
+res_dir="${script_dir}/../build/steam/$locale_dir"
 
 # Create the res directory if it doesn't exist
 mkdir -p "$res_dir"
@@ -38,24 +38,24 @@ mkdir -p "$res_dir"
 cp "$sworcery_dat_path" "$res_dir"
 
 # Run the unpack.sh script
-"${root}/unpack.sh" "$res_dir"
+"${script_dir}/unpack.sh" "$res_dir"
 
 # Copy files from locales/${locale_dir}/ to build/locales
-locales_files=$(grep -E '^locales/' "${root}/files.txt" | sed 's/^locales\///')
+locales_files=$(grep -E '^locales/' "${script_dir}/files.txt" | sed 's/^locales\///')
 while IFS= read -r file; do
-  cp "${root}/../locales/${locale_dir}/$file" "${res_dir}/locales/"
+  cp "${script_dir}/../locales/${locale_dir}/$file" "${res_dir}/locales/"
 done <<< "$locales_files"
 
 # Copy files from fonts/patched/ to build/fonts
-fonts_files=$(grep -E '^fonts/' "${root}/files.txt" | sed 's/^fonts\///')
+fonts_files=$(grep -E '^fonts/' "${script_dir}/files.txt" | sed 's/^fonts\///')
 while IFS= read -r file; do
-  cp "${root}/../fonts/patched/$file" "${res_dir}/fonts/"
+  cp "${script_dir}/../fonts/patched/$file" "${res_dir}/fonts/"
 done <<< "$fonts_files"
 
 # Run the repack.sh script
-"${root}/repack.sh" "$res_dir"
+"${script_dir}/repack.sh" "$res_dir"
 
 # Run the build-cat.py script
-"${root}/build-cat.py" "$res_dir"
+"${script_dir}/build-cat.py" "$res_dir"
 
 echo "Localization build completed for locale: $locale"
--- steam/repack.sh
+++ steam/repack.sh
@@ -6,8 +6,6 @@ script_dir=$(dirname "$(realpath "$0")")
 # Set the res_dir argument to script_dir if not provided
 res_dir=${1:-"$script_dir"}
 
-# Concatenate res_dir path to each file in files.txt
-files=$(sed "s#^#${res_dir}/#" "${script_dir}/files.txt")
-
 # Update the sworcery.dat file using 7za
-7za u "${res_dir}/sworcery.dat" ${files} -pGdHGhd4yuNF
+
+(cd "${res_dir}" && 7za u sworcery.dat @"${script_dir}/files.txt" -pGdHGhd4yuNF)
